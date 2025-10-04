a54c0388202323a450a19e134956d3279dcb9628
hubott <hugowynnthomas@gmail.com>
2025-10-04 20:32:50 +1000

    feat: Added a lambda.py file which does the API call and puts it into the database in a lambda format, so that AWS can use this on a scheduler

---
+++ lambda.py
@@ -0,0 +1,100 @@
+import requests
+import os
+import json
+import datetime
+import psycopg2
+
+DB_HOST = os.environ['DB_HOST']
+DB_NAME = os.environ['DB_NAME']
+DB_USER = os.environ['DB_USER']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+DB_PORT = os.environ['DB_PORT']
+
+conn = psycopg2.connect(
+host=DB_HOST,
+database=DB_NAME,
+user=DB_USER,
+password=DB_PASSWORD,
+port=DB_PORT
+)
+cur = conn.cursor()
+
+def lambda_handler(event, context):
+    API_KEY = os.environ['OPENWEATHER_API_KEY']
+    melbourne_lat = 37.81
+    melbourne_lon = 144.96
+    URL = f'https://api.openweathermap.org/data/3.0/onecall?lat={melbourne_lat}&lon={melbourne_lon}&appid={API_KEY}'
+    response = requests.get(URL).json()
+    fetched_at = datetime.datetime.now()
+
+    query = """
+    INSERT INTO hourly_weather (
+        fetched_at,
+        forecast_time,
+        clouds,
+        dew_point,
+        temperature,
+        feels_like,
+        humidity,
+        pop,
+        pressure,
+        UVI,
+        visibility,
+        weather,
+        wind_speed,
+        wind_deg,
+        wind_gust
+    )
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (fetched_at, forecast_time) DO UPDATE SET
+        temperature = EXCLUDED.temperature,
+        feels_like = EXCLUDED.feels_like
+    """
+
+    for i in range(len(response['hourly'])):
+        forecast_time = response['hourly'][i]['dt']
+        forecast_time_utc = datetime.datetime.fromtimestamp(forecast_time, datetime.timezone.utc)
+        forecast_time_local = forecast_time_utc.astimezone()  # Convert to local timezone
+        clouds = response['hourly'][i]['clouds']
+        dew_point = response['hourly'][i]['dew_point']
+        feels_like = int(response['hourly'][i]['feels_like']) - 273.15  # Convert from Kelvin to Celsius
+        humidity = response['hourly'][i]['humidity']
+        pop = response['hourly'][i]['pop']
+        pressure = response['hourly'][i]['pressure']
+        temperature = int(response['hourly'][i]['temp']) - 273.15  # Convert from Kelvin to Celsius
+        UVI = response['hourly'][i]['uvi']
+        visibility = response['hourly'][i]['visibility']
+        weather = response['hourly'][i]['weather'][0]['description']
+        wind_speed = response['hourly'][i]['wind_speed']
+        wind_deg = response['hourly'][i]['wind_deg']
+        wind_gust = response['hourly'][i].get('wind_gust', None)
+
+
+        values = (
+            fetched_at,
+            forecast_time_local,
+            clouds,
+            dew_point,
+            temperature,
+            feels_like,
+            humidity,
+            pop,
+            pressure,
+            UVI,
+            visibility,
+            weather,
+            wind_speed,
+            wind_deg,
+            wind_gust
+        )
+
+        cur.execute(query, values)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Weather data inserted successfully!')
+    }
