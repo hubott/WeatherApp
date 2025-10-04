3d6aae8042a5950ab4202cc9f5c555af2ebdd85b
hubott <hugowynnthomas@gmail.com>
2025-10-04 19:54:00 +1000

    feat: Time of API call is now logged to the JSON file Hourly data for the following 48hrs is uploaded to the database in data.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import requests
 import os
 from dotenv import load_dotenv
+import json
+import datetime
+
 load_dotenv()
 API_KEY = os.getenv('OPENWEATHER_API_KEY')
 melbourne_lat = 37.81
 melbourne_lon = 144.96
 URL = f'https://api.openweathermap.org/data/3.0/onecall?lat={melbourne_lat}&lon={melbourne_lon}&appid={API_KEY}'
 
+fetched_at = datetime.datetime.now()
+
 response = requests.get(URL).json()
-print(response)
+
+payload = {
+    "fetched_at": fetched_at.isoformat(),
+    "data": response
+}
+with open("sample_response.json", "w") as f:
+    json.dump(payload, f, indent=4)
---
+++ data.py
@@ -0,0 +1,93 @@
+import json
+import datetime
+import psycopg2
+from pprint import pprint
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+with open("sample_response.json", "r") as f:
+    payload = json.load(f)
+
+response = payload['data']
+fetched_at = datetime.datetime.fromisoformat(payload['fetched_at'])
+
+
+conn = psycopg2.connect(
+    host=os.getenv('DB_HOST'),
+    database=os.getenv('DB_NAME'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    port=os.getenv('DB_PORT')
+)
+
+
+cur = conn.cursor()
+print("Database connection established.")
+
+query = """
+INSERT INTO hourly_weather (
+    fetched_at,
+    forecast_time,
+	clouds,
+	dew_point,
+    temperature,
+	feels_like,
+    humidity,
+	pop,
+	pressure,
+	UVI,
+	visibility,
+	weather,
+    wind_speed,
+	wind_deg,
+	wind_gust
+)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (fetched_at, forecast_time) DO NOTHING
+"""
+
+for i in range(len(response['hourly'])):
+    forecast_time = response['hourly'][i]['dt']
+    forecast_time_utc = datetime.datetime.fromtimestamp(forecast_time, datetime.timezone.utc)
+    forecast_time_local = forecast_time_utc.astimezone()  # Convert to local timezone
+    clouds = response['hourly'][i]['clouds']
+    dew_point = response['hourly'][i]['dew_point']
+    feels_like = response['hourly'][i]['feels_like']
+    humidity = response['hourly'][i]['humidity']
+    pop = response['hourly'][i]['pop']
+    pressure = response['hourly'][i]['pressure']
+    temperature = response['hourly'][i]['temp']
+    UVI = response['hourly'][i]['uvi']
+    visibility = response['hourly'][i]['visibility']
+    weather = response['hourly'][i]['weather'][0]['description']
+    wind_speed = response['hourly'][i]['wind_speed']
+    wind_deg = response['hourly'][i]['wind_deg']
+    wind_gust = response['hourly'][i].get('wind_gust', None)
+
+
+    values = (
+        fetched_at,
+        forecast_time_local,
+        clouds,
+        dew_point,
+        temperature,
+        feels_like,
+        humidity,
+        pop,
+        pressure,
+        UVI,
+        visibility,
+        weather,
+        wind_speed,
+        wind_deg,
+        wind_gust
+    )
+
+    cur.execute(query, values)
+
+conn.commit()
+print("Data inserted successfully.")
+cur.close()
+conn.close()
+print("Database connection closed.")
